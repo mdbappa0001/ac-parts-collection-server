5cf9425be4fcf8466b7b8fd60c1b4e5ea3c78525
mdbappa0001 <abutanif366@gmail.com>
2022-05-25 02:07:45 +0600

    order loaded

--- index.js
+++ index.js
@@ -9,35 +9,44 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zelcp.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w6ifa.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
-async function run() {
-    try {
+async function run(){
+    try{
         await client.connect();
-        const serviceCollection = client.db('ac_collection').collection('services');
+        const serviceCollection = client.db('ac_collection').collection('services')
+        const bookingCollection = client.db('ac_collection').collection('bookings')
 
-        app.get('/service', async(req,res)=>{
-            const query = {};
-            const cursor = serviceCollection.find(query);
-            const services = await cursor.toArray();
-            res.send(services);
-        })
+        app.get('/service', async (req, res) => {
+        const query = {};
+        const cursor = serviceCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+     });
+
+
+     app.post('/booking', async(req, res) =>{
+        const booking = req.body;
+        const result = await bookingCollection.insertOne(booking);
+        res.send(result);
+     })
+
+
     }
-    finally {
+    finally{
 
     }
 }
 
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('Hello Form khupakupi')
+app.get("/", (req, res)=>{
+    res.send('Hello AC Collection')
 })
 
-app.listen(port, () => {
-    console.log(`Assignment 12 listening on port ${port}`)
+app.listen(port, ()=>{
+    console.log(`AC Collection listening on port ${port}`);
 })
