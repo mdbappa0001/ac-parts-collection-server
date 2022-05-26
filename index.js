ebbbd1c6a6f706cf0df3cb77fa902b36f204f342
mdbappa0001 <abutanif366@gmail.com>
2022-05-26 21:29:25 +0600

    delete method

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ async function run() {
             res.send(result);
         })
 
+        //DELETE
         app.delete('/service/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectID(id) };
